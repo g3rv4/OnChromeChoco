58ede27b5686bb745850eefe781582e2a06db8ee
Gervasio Marchand <github@gervas.io>
2020-06-28 17:06:01 -0300

    First version

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = ''
+$url64      = 'https://files.onchro.me/deploy/1.0.1/win64/OnChrome.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'OnChrome*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = '99417901CDB1DAA479FBEC25932FB00E03D742A268FFC3E1797B7AF55C4F11B1'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
